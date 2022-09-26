19dff2a5b19c94007800a4576bd143acedf0459e
michihiko-karino <michihiko.karino@gmail.com>
2022-09-26 17:56:02 +0900

    install eslint & prettier & vitest

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@vue/typescript/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: [],
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+});
